c5c02f99527e8847f05c7e62081ff3440099e921
mynameisfashanu <fashanu13@gmail.com>
2016-11-10 17:23:51 +0200

    added category ages and views

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.index,name="index"),
     url(r'^about/$',views.about,name="about"),
+    url(r'^category/(?P<category_name_url>[\w\-]+)/$',
+        views.show_category, name="show_category")
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 
 def index(request):
-    return render(request,'rango/index.html',{'boldmessage' : "I am cool"})
+    category_list = Category.objects.order_by('-likes')[:5]
+    return render(request,'rango/index.html',{'categories' : category_list })
+
+
+def show_category(request, category_name_url):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_url)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request,'rango/category.html',context_dict)
+
 
 def about(request):
     return render(request,'rango/about.html',{'message' : 'We love you!'})
